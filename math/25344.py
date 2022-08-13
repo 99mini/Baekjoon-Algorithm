75923bbe8e13d2f8e03e8bbe1865b2f39ff400d3
mini <itman390@gmail.com>
2022-08-13 11:34:45 +0900

    gcd lcm

---
+++ math/25344.py
@@ -0,0 +1,19 @@
+n = int(input())
+times = list(map(int, input().split()))
+
+def gcd(r1,r2):
+    while r2:
+       r1, r2 = r2, r1 % r2
+  
+    return r1
+
+def lcm(a,b):
+    return int(a*b / gcd(a,b))
+
+while len(times) > 1:
+    times.append(lcm(times[0],times[1]))
+    times.pop(0)
+    times.pop(0)
+    print(times)
+
+print(times[0])
