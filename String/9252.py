c597509df3bb0d7107d37bdc840ad88c1a171e15
mini <itman390@gmail.com>
2022-08-20 16:35:29 +0900

    find LCS

---
+++ String/9252.py
@@ -0,0 +1,43 @@
+# LCS 2
+# 참고: LCS 9521.py
+
+string1 = input()
+string2 = input()
+
+string1 = ' ' + string1
+string2 = ' ' + string2
+len_string1 =len(string1)
+len_string2 =len(string2)
+
+LCS = [[0 for _ in range(len_string1)] for _ in range(len_string2)]
+LCS_string = ''
+
+# LCS 길이 테이블 완성
+for i, str2 in enumerate(string2):
+    for j, str1 in enumerate(string1):
+        if i == 0 or j == 0:
+            LCS[i][j] = 0 
+        elif str1 == str2:
+            LCS[i][j] = LCS[i-1][j-1] + 1
+        else:
+            LCS[i][j] = max(LCS[i-1][j],LCS[i][j-1])
+
+# LCS 길이 출력
+print(LCS[i][j])
+
+# find LCS 
+ni = i
+nj = j
+
+while LCS[ni][nj] != 0:
+    recent = LCS[ni][nj]
+    if recent == LCS[ni-1][nj]:
+        ni -= 1
+    elif recent == LCS[ni][nj-1]:
+        nj -= 1
+    else:
+        LCS_string += string1[nj]
+        ni -= 1
+        nj -= 1 
+
+print(LCS_string[::-1])
